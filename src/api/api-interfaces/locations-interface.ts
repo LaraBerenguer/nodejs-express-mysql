51e7979d8a3b5e5937fdcc523d3d0aa5b399e912
LaraBerenguer <laraberenguer@outlook.com>
2025-01-29 13:34:46 +0100

    Added locations interface and mock data. Updated Map component to use mock locations.

---
+++ src/api/api-interfaces/locations-interface.ts
@@ -0,0 +1,5 @@
+export interface ILocations {
+    id: string;
+    lng: number;
+    lat: number;
+};
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,6 +2,7 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import './map.css';
 import { useRef, useEffect } from "react";
+import locations from '../../api/locations-mockup-data.json';
 
 const Map = () => {
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
@@ -12,8 +13,14 @@ const Map = () => {
             const map = new mapboxgl.Map({
                 container: mapContainerRef.current,
                 style: "mapbox://styles/mapbox/streets-v11",
-                center: [2.16182, 41.39725],
-                zoom: 11,
+                center: [2.17590, 41.39003],
+                zoom: 12,
+            });
+
+            locations.map(location => {
+                new mapboxgl.Marker({ color: 'black' }) //marker de ejemplo
+                    .setLngLat([location.lng, location.lat]) //cambiará con datos localización de la base de datos
+                    .addTo(map);
             });
 
             return () => { map.remove() }
