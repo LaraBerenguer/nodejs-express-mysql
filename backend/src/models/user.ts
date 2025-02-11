dfa04dbf8853d0f5941df607fa56f392564d3604
LaraBerenguer <laraberenguer@outlook.com>
2025-02-11 14:07:32 +0100

    Added primary key and auto-increment to User model.

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,6 +2,11 @@ import db from '../database/connection';
 import { DataTypes } from 'sequelize';
 
 const User = db.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     nickname: {
         type: DataTypes.STRING
     },
